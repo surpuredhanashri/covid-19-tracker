45da35fdbbeafe0addb8a95905876cb7b38c4090
dhanashri <surpuredhanashri@gmail.com>
2020-12-14 17:08:24 +0530

    diploy

---
+++ README.md
@@ -0,0 +1,12 @@
+# COVID-19 Stats Tracker!
+
+### Live Site: 
+
+![COVID-19 Tracker](https://github.com/surpuredhanashri/covid-19-tracker/blob/master/public/images/app-screenshot.png)
+
+### Info
+- Made using React.js, Chart.js, and Material-UI.
+- API used: https://covid19.mathdro.id/api
+
+### To run the project (locally on your machine):
+- Run command `npm install && npm start` in the terminal.
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+import { Cards, Chart, CountryPicker } from './components/index';
+
+import { fetchData } from './api/index';
+
+import image from './images/image.png';
+
+export default class App extends Component {
+
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+
+    const fetchedData = await fetchData();
+    // console.log(fetchedData);
+    this.setState({data: fetchedData});
+    
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    // console.log(fetchedData);
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+
+    const { data, country } = this.state;
+
+    return (
+      <div>
+        <div className={styles.container}>
+          <img className={styles.image} src={image} alt="COVID-19" />
+          <Cards data={data} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} /> 
+        </div>
+        <span className={styles.apiSpan}>
+          <span className={styles.apiTitleSpan}>API used: </span>
+          <a href="https://covid19.mathdro.id/api" className={styles.link}>
+            https://covid19.mathdro.id/api
+          </a>
+        </span>
+        <footer className={styles.footer}>
+          <span>
+            Made by 
+            <a href="https://github.com/surpuredhanashri" className={styles.link} target='_blank' rel="noopener noreferrer">
+              &nbsp;Dhanashri Surpure
+            </a>.
+          </span>
+        </footer>
+      </div>
+    );
+  }
+  
+};
